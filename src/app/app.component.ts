6b8761a497fc868ce4fbb8a20ed405e36264a6d7
ArtemFrolov <artem.frolov@valore-software.com>
2018-08-08 10:38:36 +0300

    first step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+
+interface Test {
+  valX: number;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <button type="button" (click)="changeObserver()">Change</button>
+    <button type="button" (click)="changeObserver2()">Change2</button>
+    <button type="button" (click)="changeObserver3()">Change3</button>
+  `
 })
+
 export class AppComponent {
-  title = 'app';
+  test: Test;
+  observableValue$ = new Subject<Test>();
+  newObservable$: Observable<number>;
+
+  constructor() {
+    this.test = {valX: 1};
+    this.observableValue$
+      .subscribe(observer => {
+      console.log('observer', observer);
+    });
+
+    this.newObservable$ = Observable.create(observer => {
+      observer.next('Observer Data');
+    });
+  }
+
+  changeObserver() {
+    if (this.test.valX <= 3) {
+      return this.observableValue$.next({valX: this.test.valX ++});
+    }
+
+    this.observableValue$.complete();
+  }
+
+  changeObserver2() {
+    this.observableValue$.next({valX: this.test.valX ++});
+  }
+
+  changeObserver3() {
+    this.newObservable$.subscribe(observer => {
+      console.log('observer', observer);
+    });
+  }
 }
